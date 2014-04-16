93f00781cf4504c0ff717ddfadd91d3b6cd1deb4
Kiran Kanaparthi <kkanaparthi@rh.com>
2014-04-16 00:00:16 -0700

    Added Package Level Comments.

--- src/com/resto/image/package-info.java
+++ src/com/resto/image/package-info.java
@@ -1,8 +1,10 @@
 /**
- * 
- */
-/**
- * @author kkanaparthi
+ * These classes are used to Crawl the Given Domain and Produce the Results. 
+ * <p>
+ * These classes contain the ImageCarwler Code.
  *
+ * @since 1.0
+ * @author kkanaparthi
+ * @version 1.1
  */
 package com.resto.image;
---
+++ src/com/resto/image/util/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * These classes are used to provide Utilities for the Core Classes. 
+ * <p>
+ * These classes contain the ImageCarwler Utility Code.
+ *
+ * @since 1.0
+ * @author kkanaparthi
+ * @version 1.1
+ */
+package com.resto.image.util;
